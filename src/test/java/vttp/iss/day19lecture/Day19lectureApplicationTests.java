e2b9938e7c9aad96a6625223a80b48f05312b7b6
wstan-ws <wstan.ws97@gmail.com>
2023-12-07 09:18:32 +0800

    initial commit

---
+++ src/test/java/vttp/iss/day19lecture/Day19lectureApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp.iss.day19lecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day19lectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
